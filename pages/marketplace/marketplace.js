f7ac0238300c6cba5e322bf377fcdf498ca3a64c
VinayakRB <vnyk.baghel@gmail.com>
2022-03-07 03:15:10 +0530

    added marketplace component and global styles

---
+++ pages/marketplace/marketplace.js
@@ -0,0 +1,29 @@
+import Explore from "./explore/explore"
+import Faqs from "./faqs/faqs"
+import Landing from "./landing/landing"
+import Trending from "./trending/trending"
+import { faqsData, footerData, interactionProps, trendingItems } from "./mockdata"
+import Footer from "./footer/footer"
+
+/**
+ * main component : renders different components of the app
+ *
+ * @export
+ * @return {*}
+ */
+export default function Marketplace() {
+  return (
+    <>
+      {/* landing section */}
+      <Landing></Landing>
+      {/* explore */}
+      <Explore data={interactionProps}></Explore>
+      {/* trending */}
+      <Trending data={trendingItems}></Trending>
+      {/* faqs */}
+      <Faqs data={faqsData}></Faqs>
+      {/* footer */}
+      <Footer data={footerData}></Footer>
+    </>
+  )
+}
